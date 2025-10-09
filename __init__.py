d86331d35f4a80a431ae503ada03ff9eef23ef16
Edouard Hur <edouard.hur@iguanesolutions.com>
2025-10-09 19:00:21 +0200

    write v3 format for init

--- __init__.py
+++ __init__.py
@@ -1,3 +1,17 @@
+from typing_extensions import override
+from comfy_api.latest import ComfyExtension, io
+
 from .fluxresolution import FluxResolution
 
-__all__ = [FluxResolution]
+
+class FluxResolutionExtension(ComfyExtension):
+    @override
+    async def get_node_list(self) -> list[type[io.ComfyNode]]:
+        return [
+            FluxResolution,
+        ]
+
+
+# ComfyUI calls this to load your extension and its nodes.
+async def comfy_entrypoint() -> FluxResolutionExtension:
+    return FluxResolutionExtension()
